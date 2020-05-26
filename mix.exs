0c5cd8328782aafd41b7c9f765d0ae7765e89794
Zach Daniel <zachary.s.daniel@gmail.com>
2020-05-26 09:17:55 -0400

    fix: update spandex version dependency

--- mix.exs
+++ mix.exs
@@ -64,7 +64,7 @@ defmodule SpandexPhoenix.MixProject do
       {:inch_ex, github: "rrrene/inch_ex", only: [:dev, :test]},
       {:phoenix, "~> 1.0", optional: true, only: [:dev, :test]},
       {:plug, "~> 1.3"},
-      {:spandex, "~> 2.2"}
+      {:spandex, "~> 2.2 or ~> 3.0"}
     ]
   end
 end
