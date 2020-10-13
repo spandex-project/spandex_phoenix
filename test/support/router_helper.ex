8c91eda5fd3cc1c45db3b25a822159bb049ac8f4
Kamil Kowalski <kamil@kamilkowalski.pl>
2020-10-13 19:45:29 +0200

    Handle Plug.Parsers.UnsupportedMediaTypeError
    UnsupportedMediaTypeError is raised when Plug.Parsers doesn't support
    the content-type specified in the request.
    
    Similarly to Phoenix.Router.NoRouteError, this shouldn't be treated
    as an error span, since anyone can send an unsupported media type to
    the server. Unlike NoRouteError, we can keep the resource name.
    
    This change matches the UnsupportedMediaTypeError and skips marking the
    trace as an error.

--- test/support/router_helper.ex
+++ test/support/router_helper.ex
@@ -2,8 +2,14 @@ defmodule RouterHelper do
   @moduledoc false
 
   def call(router, verb, path, opts \\ []) do
-    verb
-    |> Plug.Test.conn(path)
-    |> router.call(router.init(opts))
+    conn = Plug.Test.conn(verb, path)
+
+    conn =
+      case Keyword.get(opts, :content_type) do
+        nil -> conn
+        content_type -> Plug.Conn.put_req_header(conn, "content-type", content_type)
+      end
+
+    router.call(conn, router.init(opts))
   end
 end
