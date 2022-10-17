e0618317d458c4e1b441f2b9e7ebf5dd915da8b9
Greg Mefford <greg@gregmefford.com>
2022-10-16 22:28:27 -0400

    Prep for 1.1.0 release

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule SpandexPhoenix.MixProject do
   use Mix.Project
 
-  @version "1.0.6"
+  @version "1.1.0"
   @source_url "https://github.com/spandex-project/spandex_phoenix"
 
   def project do
